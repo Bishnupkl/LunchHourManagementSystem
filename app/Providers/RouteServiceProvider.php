5285c56989b249619805b52b5da3d103b47f1443
Bishnupkl <bishnup212@gmail.com>
2019-08-18 12:07:32 +0545

    Removing unnecessary files

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
+
 class RouteServiceProvider extends ServiceProvider
 {
     /**
